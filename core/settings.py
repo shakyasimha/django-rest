89da8cc47927a676da9464bf3cffdb56cd029049
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2023-06-21 13:26:31 +0545

    created Snippet app, updated serializers and models

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'user.apps.UserConfig'
+    'user.apps.UserConfig',
+    'snippet.apps.SnippetConfig'
 ]
 
 MIDDLEWARE = [
