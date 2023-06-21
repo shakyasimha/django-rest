2377e9a38906c738c3bb2191fb3bd415f8b0838f
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2023-06-21 13:16:13 +0545

    Revert "added ViewSets and included path for rest framework"
    This reverts commit 9e3bf9b667847b01da50772d8976c23122831525.

--- core/urls.py
+++ core/urls.py
@@ -15,17 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include 
-from rest_framework import routers
-from user import views
-
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_frameworks.urls', namespace='rest_framework'))
 ]
