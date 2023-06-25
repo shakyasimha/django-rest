1ef705925f9ec2f996c6371479e092f226acf93d
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2023-06-25 16:12:51 +0545

    added views for authors and publishers

--- mysite/views.py
+++ mysite/views.py
@@ -1,17 +1,39 @@
 from django.shortcuts import render
 from rest_framework import generics
 
-from .models import Books
-from .serializers import BookSerializer
+from .models import Books, Author, Publisher
+from .serializers import BookSerializer, AuthorSerializer, PublisherSerializer
 
 # Create your views here.
 
-# For listing and creating -> LC
-class BookLCView(generics.ListCreateAPIView):
+# For Listing and Creating = LC
+# For Retrieving, Updating and Deleting = RUD
+
+# For books:
+class BookListView(generics.ListAPIView):
     queryset = Books.objects.all()
     serializer_class = BookSerializer
 
-# For retrieving, updating and deleting/destroying -> RUD
-class BookRUDView(generics.RetrieveUpdateDestroyAPIView):
+class BookRetrieveView(generics.RetrieveAPIView):
     queryset = Books.objects.all()
     serializer_class = BookSerializer
+
+
+# For authors:
+class AuthorListView(generics.ListAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer 
+
+class AuthorRetrieveView(generics.RetrieveAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+# For publishers:
+class PublisherListView(generics.ListAPIView):
+    queryset = Publisher.objects.all()
+    serializer_class = PublisherSerializer
+
+class PublisherRetrieveView(generics.RetrieveAPIView):
+    queryset = Publisher.objects.all()
+    serializer_class = PublisherSerializer
