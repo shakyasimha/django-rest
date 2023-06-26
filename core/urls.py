77f6d0bdd4a14a8197b284056c044b504e2ad918
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2023-06-26 11:39:31 +0545

    changed urls.py

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from mysite import views 
 
@@ -26,5 +26,6 @@ urlpatterns = [
     path('author/', views.AuthorListView.as_view()),
     path('author/<int:pk>/', views.AuthorDetailView.as_view()),
     path('publisher/', views.PublisherListView.as_view()),
-    path('publisher/<int:pk>/', views.PublisherDetailView.as_view())
+    path('publisher/<int:pk>/', views.PublisherDetailView.as_view()),
+    path('api-auth/', include('mysite.urls'))
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('books/', views.BookListView.as_view()),
+    path('books/<int:pk>/', views.BookDetailView.as_view()),
+    path('author/', views.AuthorListView.as_view()),
+    path('author/<int:pk>/', views.AuthorDetailView.as_view()),
+    path('publisher/', views.PublisherListView.as_view()),
+    path('publisher/<int:pk>/', views.PublisherDetailView.as_view()),
 ]
